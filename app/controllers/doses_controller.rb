830e49eadd95c1ebfb48c60e7b1839757986e0a5
Jocelyn Duquesnoy <jocelyn.duquesnoy@yahoo.fr>
2020-08-13 16:15:51 +0200

    back finished

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,47 +1,31 @@
 class DosesController < ApplicationController
-#   def destroy
-#   end
-
-#   def new
-  
-#   end
-
-#   def create
-#   end
-# end
-
-before_action :set_garden, only: :create
-before_action :set_dose, only: :destroy
-
-def create
-  @dose = Dose.new(dose_params)
-  @dose.cocktail = @cocktail
-
-  if @dose.save
-    redirect_to cocktail_path(@cocktail)
-  else
-    render 'cocktails/show'
+ 
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path(@dose.cocktail)
   end
-end
 
-def destroy
-  cocktail = @dose.cocktail
-  @dose.destroy
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+  
+  end
 
-  redirect_to cocktail_path(cocktail)
-end
+  def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render "new"
+    end
+  end
 
-private
+  private
 
-def set_dose
-  @dose = Dose.find(params[:id])
-end
-
-def set_cocktail
-  @cocktail = cocktail.find(params[:cocktail_id])
-end
-
-def dose_params
-  params.require(:dose).permit(:description)
-end
+  def dose_params
+    params.require(:dose).permit(:description,:ingredient_id)
+  end
 end
