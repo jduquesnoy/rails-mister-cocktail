5f1dddc8641f70de2278d8967e86282f228a6d43
Jocelyn Duquesnoy <jocelyn.duquesnoy@yahoo.fr>
2020-08-14 11:54:01 +0200

    ajustement back

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'doses/destroy'
-  get 'doses/create'
   resources :cocktails, only: [ :index, :show, :new, :create ] do
     # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
     resources :doses, only: [:create, :new]
